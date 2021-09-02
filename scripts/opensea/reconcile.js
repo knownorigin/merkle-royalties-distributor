5af1501d9f190d5c46ce18bbf8ad61abfecb5d2d
Vincent de Almeida <vincent@decentralisedengineeringltd.co.uk>
2021-09-02 12:20:17 +0100

    hre and shell script for running reconcie script

--- scripts/opensea/reconcile.js
+++ scripts/opensea/reconcile.js
@@ -1,18 +1,25 @@
 const _ = require('lodash')
 const axios = require('axios')
 const fs = require('fs');
-const vaultABI = require('../../artifacts/contracts/MerkleVault.sol/MerkleVault.json').abi
+const vaultABI = require('../../artifacts/contracts/MerkleVault.sol/MerkleVault.json').abi;
 
 task("reconcile", "Reconcile")
-  //.addParam('vaultAddress', 'Address of the vault')
+  .addParam('vaultAddress', 'Address of the vault')
   //.addParam('fromBlock', 'From Block')
   //.addParam('toBlock', 'To Block')
-  .setAction(async taskArgs => {
+  .setAction(async (taskArgs, hre) => {
+    const {
+      vaultAddress,
+      //fromBlock,
+      //toBlock
+    } = taskArgs
+
     console.log(`Starting task...`);
 
-    const [deployer] = await ethers.getSigners()
+    const {name: network} = hre.network;
+    console.log(`Running on network [${network}]`);
 
-    const vaultAddress = '0x19F794cd47d7816cbEeC19E363bc562B51066cd6'
+    const [deployer] = await ethers.getSigners()
 
     const vault = new ethers.Contract(
       vaultAddress,
@@ -31,24 +38,24 @@ task("reconcile", "Reconcile")
     const {claims} = data
     const beneficiaries = Object.keys(claims)
 
-    console.log('beneficiaries.length', beneficiaries.length)
+    console.log('Current tree total beneficiaries', beneficiaries.length)
 
     let eventFilter = vault.filters.TokensClaimed()
     let events = await vault.queryFilter(eventFilter) // todo pass in to and from block
 
-    const provider = ethers.providers.getDefaultProvider('rinkeby')
+    const provider = ethers.providers.getDefaultProvider(network)
 
     const claimants = await Promise.all(events.map(async ({transactionHash}) => {
       const tx = await provider.getTransaction(transactionHash)
       return ethers.utils.getAddress(tx.from)
     }))
 
-    console.log('claimants.length', claimants.length)
+    console.log('Number of claimants from current tree', claimants.length)
     const unclaimedBeneficiaries = beneficiaries.filter(beneficiary => {
       return !_.includes(claimants, beneficiary)
     })
 
-    console.log('unclaimedBeneficiaries.length', unclaimedBeneficiaries.length)
+    console.log('Number of beneficiaries that have not claimed', unclaimedBeneficiaries.length)
 
     // for ETH based payments, we encode the token as the zero address in the tree
     const token = '0x0000000000000000000000000000000000000000';
---
+++ scripts/opensea/reconcile.sh
@@ -0,0 +1,1 @@
+yarn hardhat reconcile --network rinkeby --vault-address "0x19F794cd47d7816cbEeC19E363bc562B51066cd6"
